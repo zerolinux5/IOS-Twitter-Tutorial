e6c06e8996f6a57a6a0ff5411b9cad0cb12e26d1
Jesus Magana <maganazero5@gmail.com>
2014-06-30 21:08:43 -0700

    Initial Commit

---
+++ SimpleTweet/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SimpleTweet
+//
+//  Created by Jesus Magana on 6/30/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SimpleTweet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleTweet
+//
+//  Created by Jesus Magana on 6/30/14.
+//  Copyright (c) 2014 ZeroLinux5. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
